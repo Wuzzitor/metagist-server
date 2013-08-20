e921eecab4901a1469ea67f19efb736d232778f7
Daniel Pozzi <bonndan76@googlemail.com>
2013-08-20 19:45:42 +0200

    including the version file in bundle class

--- src/Metagist/ServerBundle/MetagistServerBundle.php
+++ src/Metagist/ServerBundle/MetagistServerBundle.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MetagistServerBundle extends Bundle
 {
+    public function boot()
+    {
+        parent::boot();
+        require_once $this->getPath(). '/version.php';
+    }
 }
