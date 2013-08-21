ce6abd29ab3405e0d5a523cca5ddce399e60ec4f
Daniel Pozzi <bonndan76@googlemail.com>
2013-08-21 21:28:04 +0200

    dep repo provides consumers

--- src/Metagist/ServerBundle/Entity/DependencyRepository.php
+++ src/Metagist/ServerBundle/Entity/DependencyRepository.php
@@ -15,11 +15,23 @@ class DependencyRepository extends EntityRepository
      * Retrieves all dependencies info for the given package.
      * 
      * @param \Metagist\Package $package
-     * @return \Doctrine\Common\Collections\Collection
+     * @return Dependency[]
      */
     public function byPackage(Package $package)
     {
         $result = $this->findBy(array('package' => $package));
+        return new ArrayCollection($result);
+    }
+    
+    /**
+     * Retrieves all dependencies consuming the given package (reverse search)
+     * 
+     * @param \Metagist\Package $package
+     * @return Dependency[]
+     */
+    public function getConsumersOf(Package $package)
+    {
+        $result = $this->findBy(array('dependencyIdentifier' => $package->getIdentifier()));
         return new ArrayCollection($result);
     }
 }
